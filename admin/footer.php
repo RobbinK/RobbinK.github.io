077526a462a136247f697f898a242fee7531cbbd
RobbinK <kalaamalho2@gmail.com>
2017-05-15 23:12:06 -0600

    Removed everything

--- admin/footer.php
+++
@@ -1,9 +0,0 @@
-<?php if(@$isLogin and $userinfo->role == "admin"): ?>
-<hr>
-<footer class="pager" style="bottom:0px;">
-        <p><?php echo FOOTER; ?></p>
-</footer>
-
-</body>
-</html>
-<?php endif;?>
--- footer.php
+++
@@ -1,9 +0,0 @@
-<?php if(@$isLogin and $userinfo->role == "user"): ?>
-
-<hr style="margin-top:0px;">
-<footer class="pager" style="bottom:0px;">
-        <p><?php echo FOOTER;?></p>
-</footer>
-</body>
-</html>
-<?php endif;?>
