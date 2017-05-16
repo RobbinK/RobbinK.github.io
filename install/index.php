2b0bbe85e0ddcfe14b9c4ae026dd2086360252b8
RobbinK <kalaamalho2@gmail.com>
2017-05-15 23:10:46 -0600

    Added install

---
+++ install/index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<meta name="robots" content="noindex">
+<meta name="googlebot" content="noindex">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Install</title>
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="../css/bootstrap-responsive.css" />
+</head>
+<body>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script type="text/javascript" src="../js/bootstrap.js"></script>
+
+<article class="container">
+<section class="form-actions pager">
+            
+            <form action="install.php" method="post">
+                <legend>Install</legend>
+
+                    <div class="input-prepend"><span class="add-on">Email &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="icon-envelope"></i></span><input placeholder="Email" name="email"  id="" value="" class="span2 input-block-level" type="email" required="required"></div>
+                    
+                    <div class="input-prepend"><span class="add-on">Firstname &nbsp;<i class="icon-user"></i></span><input placeholder="Firstname" name="name" id="" value="" class="span2 input-block-level" type="text" required="required"></div>
+                       
+                    <div class="input-prepend"><span class="add-on">Lastname &nbsp;<i class="icon-user"></i></span><input placeholder="Lastname" name="lname" id="" value="" class="span2 input-block-level" type="text" required="required"></div>
+    
+                    <div class="input-prepend"><span class="add-on">Password &nbsp;<i class="icon-lock"></i></span><input placeholder="Password" name="password"  id="" class="span2 input-block-level" type="password" required="required"></div>
+                
+                <input class="btn btn-info"  name="install" value="Install" class="btn btn-primary" type="submit">
+  
+            </form>
+        
+</section>    	
+</article>
+</body>
+</html>
