2ff27ec0ab96c77ee78f140e019fbb5d8f40b653
RobbinK <kalaamalho2@gmail.com>
2017-06-30 00:15:48 -0600

    Mineplex nav

--- games/js/main.js
+++ games/js/main.js
@@ -1,108 +1,60 @@
+/*
+-------------------------------------
+------------------------------------- The image changue  every 10 Secons
+-------------------------------------
+-------------------------------------
+*/
 
-function main() {
+var Cont = 0;
+function inic(){
+if(Cont % 2 != 0){
+  document.querySelector('.img_1').style.opacity = '0';
+  document.querySelectorAll('.img_1')[1].style.opacity = '0';
 
-(function () {
-   'use strict';
-   
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 40
-            }, 900);
-            return false;
-          }
-        }
-      });
+setTimeout(function (){
+document.querySelector('.img_1').className = 'img_1 img_d_n';
+    document.querySelectorAll('.img_1')[1].className = 'img_1 img_d_n';
 
-	// affix the navbar after scroll below header
-$('#nav').affix({
-      offset: {
-        top: $('header').height()
-      }
-});	
+  document.querySelector('.img_2').className = 'img_2 img_d_b';
 
-	// skills chart
-	$(document).ready(function(e) {
-	//var windowBottom = $(window).height();
-	var index=0;
-	$(document).scroll(function(){
-		var top = $('#skills').height()-$(window).scrollTop();
-		console.log(top)
-		if(top<-300){
-			if(index==0){	
-			
-				$('.chart').easyPieChart({
-					easing: 'easeOutBounce',
-					onStep: function(from, to, percent) {
-						$(this.el).find('.percent').text(Math.round(percent));
-					}
-				});
-			
-				}
-			index++;
-		}
-	})
-	//console.log(nagativeValue)
-	});
-	
-	// Hide nav on click
-  $(".navbar-nav li a").click(function (event) {
-    // check if window is small enough so dropdown is created
-    var toggle = $(".navbar-toggle").is(":visible");
-    if (toggle) {
-      $(".navbar-collapse").collapse('hide');
-    }
-  });
+  document.querySelectorAll('.img_2')[1].className = 'img_2 img_d_b';
+  
+},500);  
+  setTimeout(function (){
+document.querySelector('.img_2').style.opacity = '0.2';  
+  document.querySelectorAll('.img_2')[1].style.opacity = '1';
+ 
+  },600);
+    Cont++;
+}else{
 
-  	// Portfolio isotope filter
-    $(window).load(function() {
-        var $container = $('.portfolio-items');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-        });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
+  document.querySelector('.img_2').style.opacity = '0';
+  document.querySelectorAll('.img_2')[1].style.opacity = '0';
 
-    });
-	
-	  	
-    // CounterUp
-	$(document).ready(function( $ ) {
-		if($("span.count").length > 0){	
-			$('span.count').counterUp({
-					delay: 10, // the delay time in ms
-			time: 1500 // the speed time in ms
-			});
-		}
-	});
-	
-  	// Pretty Photo
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});	
+setTimeout(function (){
 
-}());
+  document.querySelector('.img_2').className = 'img_2 img_d_n';
+  document.querySelectorAll('.img_2')[1].className = 'img_2 img_d_n';
 
+  document.querySelector('.img_1').className = 'img_1 img_d_b';
+  document.querySelectorAll('.img_1')[1].className = 'img_1 img_d_b';
 
+},500);  
+  
+  setTimeout(function (){
+document.querySelector('.img_1').style.opacity = '0.2';
+document.querySelectorAll('.img_1')[1].style.opacity = '1';
+  },600);  
+  Cont++;
+}   }
+
+window.onload = function (){  
+  inic();  document.querySelector('.cont_text_img').className = "cont_text_img cont_text_img_act";
+setTimeout(function(){
+  document.querySelector('.cont_img_frond').className = "cont_img_frond  cont_img_frond_active";
+},1700);
+  
+  
 }
-main();
+
+setInterval(function(){ inic() }, 10000);
